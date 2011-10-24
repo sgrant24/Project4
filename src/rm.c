4c64042595e4f0697a199c3cdae25e64b597eca6
Sean Grant <sgrant24@ucla.edu>
2011-10-23 23:35:18 -0700

    commit 2

--- src/rm.c
+++ src/rm.c
@@ -28,7 +28,6 @@
 #include "argmatch.h"
 #include "error.h"
 #include "quote.h"
-#include "quotearg.h"
 #include "remove.h"
 #include "root-dev-ino.h"
 #include "yesno.h"
@@ -97,33 +96,6 @@ static enum interactive_type const interactive_types[] =
   interactive_always, interactive_always
 };
 ARGMATCH_VERIFY (interactive_args, interactive_types);
-
-/* Advise the user about invalid usages like "rm -foo" if the file
-   "-foo" exists, assuming ARGC and ARGV are as with `main'.  */
-
-static void
-diagnose_leading_hyphen (int argc, char **argv)
-{
-  /* OPTIND is unreliable, so iterate through the arguments looking
-     for a file name that looks like an option.  */
-  int i;
-
-  for (i = 1; i < argc; i++)
-    {
-      char const *arg = argv[i];
-      struct stat st;
-
-      if (arg[0] == '-' && arg[1] && lstat (arg, &st) == 0)
-        {
-          fprintf (stderr,
-                   _("Try `%s ./%s' to remove the file %s.\n"),
-                   argv[0],
-                   quotearg_n_style (1, shell_quoting_style, arg),
-                   quote (arg));
-          break;
-        }
-    }
-}
 
 void
 usage (int status)
@@ -309,7 +281,6 @@ main (int argc, char **argv)
         case_GETOPT_HELP_CHAR;
         case_GETOPT_VERSION_CHAR (PROGRAM_NAME, AUTHORS);
         default:
-          diagnose_leading_hyphen (argc, argv);
           usage (EXIT_FAILURE);
         }
     }
